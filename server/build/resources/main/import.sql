4182e1504a88048d9ba26998e2088ccc2bb70dbd
JanSvoboda6 <jansvoboda596@gmail.com>
2022-04-12 15:53:57 +0200

    Untrack .idea/, build/, .gradle/ folders

--- server/build/resources/main/import.sql
+++
@@ -1,6 +0,0 @@
--- INSERT INTO project (id, name, first_label, second_label, first_label_folder, second_label_folder, selected_model) VALUES(10, 'Machine Learning Model #1', 'ones', 'zeros', '/test_folder_a', '/test_folder_b', 'Support Vector Machines');
--- INSERT INTO project VALUES(2, 'Machine Learning Model #2');
--- INSERT INTO project VALUES(3, 'Machine Learning Model #3');
-
-INSERT INTO role(name) VALUES('ROLE_USER');
-INSERT INTO role(name) VALUES('ROLE_ADMIN');
