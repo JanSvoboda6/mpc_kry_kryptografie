b5ccf978af856529f70aa761920c60b4a39ec5d6
JanSvoboda6 <jansvoboda596@gmail.com>
2022-04-16 18:11:32 +0200

    Enabling private routes

--- client/src/helpers/PrivateRoute.jsx
+++ client/src/helpers/PrivateRoute.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-//import {store} from '../redux/store';
 
 const PrivateRoute = ({component: Component, ...rest}) => {
     return (
         <Route {...rest} render={props => (
-            <Component {...props} />
-            // localStorage.user ? <Component {...props} /> : <Redirect to="/login" />
+            localStorage.user ? <Component {...props} /> : <Redirect to="/login" />
         )} />
     );
 };
