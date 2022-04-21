247dd8ba375cdc38ff9954d0a065ed23c04bce9e
Ondřej Buš <hwired@elementarydesign.net>
2022-04-21 16:19:04 +0200

    Removed certificated + described some of the code

--- CertificationAuthority/src/CryptoCAMain.java
+++ CertificationAuthority/src/CryptoCAMain.java
@@ -1,21 +1,31 @@
 public class CryptoCAMain
 {
+	/**
+	 * The entry method of the Certificate Generator.
+	 * This app will generate certificates and private keys for both - the Certification Authority and the Client.
+	 * The generated certificate for the Certification Authority will need to be imported on the machines that wish to visit the client's website.
+	 * Normally, the well known CA certificates are already installed on the computers with whatever operating systems are they running.
+	 * But because we are not well known, we need to import the generated CA's certificate on machines manually.
+	 * 
+	 * @param args  Command line arguments - we are not using any.
+	 */
     public static void main(String[] args)
     {
+    	// Create a new Certification Authority
     	CA ca = new CA();
     	
-    	RootCert caCert = ca.CreateSelfSignedCACertificate("*.customca.org", "CN=Root-CustomCA, O=CustomCA", 10, "CACert.crt", "CACert.key");
-        CSRWithPrivKey csr = ca.RegisterClient("*.securestorage.website", "SecureStorage", "undefined dept.", "Moravsky", "CZ");
-        Cert clientCert = ca.CreateClientCertificate("*.securestorage.website", caCert, csr, "ServerCert.crt", "ServerCertPriv.key");
-        boolean isCertValid = ca.Validate(clientCert, caCert);
+    	// Create a certificate for the Certification Authority
+    	RootCert caCert = ca.createSelfSignedCACertificate("*.customca.org", "CN=Root-CustomCA, O=CustomCA", 10, "CACert.crt", "CACert.key");
+        // Register a new client with the Certification Authority
+    	CSRWithPrivKey csr = ca.registerClient("*.securestorage.website", "SecureStorage", "undefined dept.", "Moravsky", "CZ");
+        // Generate a certificate for the client signed by the Certification Authority
+    	Cert clientCert = ca.createClientCertificate("*.securestorage.website", caCert, csr, "ServerCert.crt", "ServerCertPriv.key");
         
+    	// Validate the client certificate just in case something in the process went wrong
+    	boolean isCertValid = ca.validate(clientCert, caCert);
         if (isCertValid)
-        {
         	System.out.println("Generated certificate is valid.");
-        }
         else
-        {
         	System.out.println("Generated certificate is not valid.");
-        }
     }
 } 
